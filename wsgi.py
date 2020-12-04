000e440a74af1df0d13b996bbe7387579786a4f5
Mari1-m <75465335+Mari1-m@users.noreply.github.com>
2020-12-04 11:24:20 +0600

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mari project.
+WSGI config for viceversa project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mari.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viceversa.settings')
 
 application = get_wsgi_application()
